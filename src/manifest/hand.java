ad486884215512349cb4d02d73a605da9c3fe2a2
jimender2 <35303639+jimender2@users.noreply.github.com>
2020-06-11 16:53:07 -0400

    Fix some small bugs in the hand creation. Adjust constuctor to read name and cards from one user string. Additionally add in the beginning of values to compare to match a hand to another hand.

--- src/manifest/hand.java
+++ src/manifest/hand.java
@@ -8,18 +8,77 @@ public class hand {
 	ArrayList<card> cards;
 	int[] value;
 
-	public hand(String name, String str) {
-		this.name = name;
+	/**
+	 * Default Constructor
+	 * 
+	 * @param name
+	 * @param str
+	 */
+	public hand(String str) {
 
-		value = new int[8];
+		String[] strSplit = str.split(": ");
+		this.name = strSplit[0];
 
-		String[] strSplit = str.split(" ");
+		strSplit = strSplit[1].split(" ");
 
+		this.cards = new ArrayList<card>();
+		// Add cards to the hand (an array list for simplicity)
 		for (String s : strSplit) {
-			cards.add(new card(s));
+			this.cards.add(new card(s));
 		}
+
+		// Count the number of each type of card in my hand (ie 2 2s 1 4 and 1K)
+		// This preps for the pair, two pair, three of a kind, and four of a kind
+
+		// index 2 = 2
+		// index 3 = 3
+		// etc...
+		// index 0 and 1 should be blank. This is to map the numbers to their
+		// number easier
+		int[] count = new int[15];
+		for (int i = 0; i < 15; i++) {
+			count[i] = 0;
+		}
+
+		for (card card : cards) {
+			count[card.getValue()]++;
+		}
+
+		boolean pair = false;
+		boolean twoPair = false;
+		boolean threeOfAKind = false;
+		boolean fourOfAKind = false;
+
+		int pairNum;
+		int twoPairNum;
+		int threeOfAKindNum;
+		int fourOfAKindNum;
+
+		for (int i : count) {
+			if (i == 2) {
+				if (!pair) {
+					pair = true;
+					pairNum = i;
+				} else {
+					twoPair = true;
+					twoPairNum = i;
+				}
+			} else if (i == 3) {
+				threeOfAKind = true;
+				threeOfAKindNum = i;
+			} else if (i == 4) {
+				fourOfAKind = true;
+				fourOfAKindNum = i;
+			}
+		}
+
 	}
 
+	/**
+	 * getPlayerName
+	 * 
+	 * @return Returns the players name (ie Black or White)
+	 */
 	String getPlayerName() {
 		return name;
 	}
@@ -31,4 +90,19 @@ public class hand {
 		}
 	}
 
+	int compareTo(hand other) {
+		int returnInt = 0;
+
+		// Change the values to return sooner
+		for (int i = 0; i < value.length; i++) {
+			if (this.value[i] < other.value[i]) {
+				returnInt = -1;
+				break;
+			} else if (this.value[i] > other.value[i]) {
+				returnInt = 1;
+				break;
+			}
+		}
+		return returnInt;
+	}
 }
