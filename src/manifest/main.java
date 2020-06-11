c9e7b5d8d3dc88cb8da652e70a1924f3d07c0b35
jimender2 <35303639+jimender2@users.noreply.github.com>
2020-06-11 16:52:39 -0400

    Add a main method to start testing one hand creation.

---
+++ src/manifest/main.java
@@ -0,0 +1,16 @@
+package manifest;
+
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter into my poker kingdom");
+
+		Scanner scan = new Scanner(System.in);
+
+		hand h1 = new hand(scan.nextLine());
+	}
+
+}
