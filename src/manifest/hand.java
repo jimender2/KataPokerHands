4cdef7722d9655f0a4d54de0a152678f5608553f
jimender2 <35303639+jimender2@users.noreply.github.com>
2020-06-12 12:45:21 -0400

    Add in version 1 of all of the comparisons that I need to do to see who it the round winner.

--- src/manifest/hand.java
+++ src/manifest/hand.java
@@ -26,120 +26,6 @@ public class hand {
 		for (String s : strSplit) {
 			this.cards.add(new card(s));
 		}
-
-		// Count the number of each type of card in my hand (ie 2 2s 1 4 and 1K)
-		// This preps for the pair, two pair, three of a kind, and four of a kind
-
-		// index 2 = 2
-		// index 3 = 3
-		// etc...
-		// index 0 and 1 should be blank. This is to map the numbers to their
-		// number easier
-		int[] count = new int[15];
-		for (int i = 0; i < 15; i++) {
-			count[i] = 0;
-		}
-
-		for (card card : cards) {
-			count[card.getValue()]++;
-		}
-
-		boolean pair = false;
-		boolean twoPair = false;
-		boolean threeOfAKind = false;
-		boolean fourOfAKind = false;
-
-		int pairNum = 0;
-		int twoPairNum = 0;
-		int threeOfAKindNum = 0;
-		int fourOfAKindNum = 0;
-
-		for (int i : count) {
-			if (i == 2) {
-				if (!pair) {
-					pair = true;
-					pairNum = i;
-				} else {
-					twoPair = true;
-					twoPairNum = i;
-				}
-			} else if (i == 3) {
-				threeOfAKind = true;
-				threeOfAKindNum = i;
-			} else if (i == 4) {
-				fourOfAKind = true;
-				fourOfAKindNum = i;
-			}
-		}
-
-		boolean fullHouse = false;
-		int fullHouseNum;
-		// Now to check if it is Full house
-		if (twoPair && threeOfAKind) {
-			fullHouse = true;
-			fullHouseNum = threeOfAKindNum;
-		}
-
-		// Check for a Flush
-		boolean straight = false;
-		int straightNum = 0;
-		int consecutive = 0;
-		for (int i : count) {
-			if (i == 1) {
-				consecutive = consecutive + 1;
-			} else if (i > 1) {
-				// Does not make sense to keep running through if we already have
-				// consecutive cards since there are multiple cards of this value
-				// in this pile
-				break;
-			} else if (i == 0 && consecutive != 0) {
-				// Does not make sense to keep running through if we already have
-				// consecutive cards since there are no cards in this pile and
-				// we already have consecutive cards
-				break;
-			}
-
-			if (consecutive == 5) {
-				straight = true;
-				straightNum = i;
-				break;
-			}
-		}
-
-		// Flush
-		boolean flush = false;
-		int[] suit = new int[4];
-		for (int i = 0; i < 4; i++) {
-			suit[i] = 0;
-		}
-
-		for (card card : cards) {
-			switch (card.getSuit()) {
-			case 'D':
-				suit[0] = suit[0] + 1;
-				break;
-			case 'H':
-				suit[1] = suit[1] + 1;
-				break;
-			case 'C':
-				suit[2] = suit[2] + 1;
-				break;
-			case 'S':
-				suit[3] = suit[3] + 1;
-				break;
-			}
-		}
-
-		for (int i : suit) {
-			if (i == 5) {
-				flush = true;
-			}
-		}
-
-		boolean straightFlush = false;
-		if (straight && flush) {
-			straightFlush = true;
-		}
 	}
 
 	/**
@@ -158,19 +44,106 @@ public class hand {
 		}
 	}
 
-	int compareTo(hand other) {
-		int returnInt = 0;
+	String compareTo(hand other) {
+		score h1 = new score(this.cards);
+		score h2 = new score(other.cards);
 
-		// Change the values to return sooner
-		for (int i = 0; i < value.length; i++) {
-			if (this.value[i] < other.value[i]) {
-				returnInt = -1;
-				break;
-			} else if (this.value[i] > other.value[i]) {
-				returnInt = 1;
-				break;
+		if (h1.straightFlush) {
+			return this.getPlayerName() + " wins. - with straight flush.";
+		} else if (h2.straightFlush) {
+			return other.getPlayerName() + " wins. - with straight flush.";
+		} else if (h1.fourOfAKind && h2.fourOfAKind) {
+			if (h1.fourOfAKindNum > h2.fourOfAKindNum) {
+				return this.getPlayerName() + " wins. - with four of a kind";
+			} else {
+				return other.getPlayerName() + " wins. - with four of a kind";
 			}
+		} else if (h1.fourOfAKind) {
+			return this.getPlayerName() + " wins. - with four of a kind";
+		} else if (h2.fourOfAKind) {
+			return other.getPlayerName() + " wins. - with four of a kind";
+		} else if (h1.fullHouse && h2.fullHouse) {
+			if (h1.fullHouseNum > h2.fullHouseNum) {
+				return this.getPlayerName() + " wins. - with a full house";
+			} else {
+				return other.getPlayerName() + " wins. - whth a full house";
+			}
+		} else if (h1.fullHouse) {
+			return this.getPlayerName() + " wins. - with a full house.";
+		} else if (h2.fullHouse) {
+			return other.getPlayerName() + " wins. - with a full house";
+		} else if (h1.flush && h2.flush) {
+			// todo
+		} else if (h1.flush) {
+			return this.getPlayerName() + " wins. - with a flush";
+		} else if (h2.flush) {
+			return other.getPlayerName() + " wins. - with a flush";
+		} else if (h1.straight && h2.straight) {
+			if (h1.straightNum > h2.straightNum) {
+				return this.getPlayerName() + " wins. - with a straight";
+			} else {
+				return other.getPlayerName() + " wins. - with a straight";
+			}
+		} else if (h1.straight) {
+			return this.getPlayerName() + " wins. - with a straight";
+		} else if (h2.straight) {
+			return other.getPlayerName() + " wins. - with a straight";
+		} else if (h1.threeOfAKind && h2.threeOfAKind) {
+			if (h1.threeOfAKindNum > h2.threeOfAKindNum) {
+				return this.getPlayerName() + " wins. - with a three of a kind";
+			} else {
+				return other.getPlayerName() + " wins. - with a three of a kind";
+			}
+		} else if (h1.threeOfAKind) {
+			return this.getPlayerName() + " wins. - with a three of a kind";
+		} else if (h2.threeOfAKind) {
+			return other.getPlayerName() + " wins. - with a three of a kind";
+		} else if (h1.twoPair && h2.twoPair) {
+			int h1num = 0;
+			if (h1.twoPairNum >= h1.pairNum) {
+				h1num = h1.twoPairNum;
+			} else {
+				h1num = h1.pairNum;
+			}
+
+			int h2num = 0;
+			if (h2.twoPairNum >= h2.pairNum) {
+				h2num = h2.twoPairNum;
+			} else {
+				h2num = h2.pairNum;
+			}
+
+			if (h1num > h2num) {
+				return this.getPlayerName() + " wins. - with a two pair";
+			} else {
+				return other.getPlayerName() + " wins. - with a two pair";
+			}
+		} else if (h1.twoPair) {
+			return this.getPlayerName() + " wins. - with a two pair";
+		} else if (h2.twoPair) {
+			return other.getPlayerName() + " wins. - with a two pair";
+		} else if (h1.pair && h2.pair) {
+			if (h1.pairNum > h2.pairNum) {
+				return this.getPlayerName() + " wins. - with a pair";
+			} else {
+				return other.getPlayerName() + " wins. - with a two pair";
+			}
+		} else if (h1.pair) {
+			return this.getPlayerName() + " wins. - with a two pair";
+		} else if (h2.pair) {
+			return other.getPlayerName() + " wins. - with a two pair";
+		} else {
+			for (int i = 0; i < h1.highNum.length; i++) {
+				int len = h1.highNum.length - 1;
+				if (h1.highNum[len - i] > h2.highNum[len - i]) {
+					return this.getPlayerName() + " wins. - with high card: " + card.getName(h1.highNum[len - i]);
+				} else if (h1.highNum[len - i] < h2.highNum[len - i]) {
+					return other.getPlayerName() + " wins. - with high card: " + card.getName(h2.highNum[len - i]);
+				}
+			}
+			return "Tie.";
 		}
-		return returnInt;
+
+		return "";
 	}
 }
